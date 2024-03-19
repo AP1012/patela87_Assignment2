4f079c501f99814d1d46c8b88b2c9de50b5a4738
patela87 <patela87@SCE072>
2024-03-19 17:37:07 -0600

    Add project files.

---
+++ patela87_Assignment2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int evenSum = 0;
+        int oddSum = 0;
+
+             for (int i = 0; i < 10; i++)
+        {
+  
+            if (i % 2 == 0)
+            {
+                Console.WriteLine($"{i} is an even.");
+   
+                evenSum += i;
+            }
+            else
+            {
+                Console.WriteLine($"{i} is an odd.");
+               
+                oddSum += i;
+            }
+        }
+                
+        Console.WriteLine("Sum of even numbers: " + evenSum);
+        Console.WriteLine("Sum of odd numbers: " + oddSum);
+    }
+}
